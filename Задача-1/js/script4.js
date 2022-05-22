1f1ca0cbdb24ffa60a8e54de32a213247f1545b6
Ayzadaa <ayzada.baichabaeva.kk@gmail.com>
2022-05-22 14:39:20 +0600

    Update

---
+++ Задача-1/js/script4.js
@@ -0,0 +1,7 @@
+let a = Number(prompt("Сколько штук сувениров купили?"));
+
+let b = Number(prompt("Сколько штук безделушек купили?"));
+
+let result = a * 75 + b * 112;
+
+document.write("<br><strong>Общий вес посылки:</strong>" + result);
